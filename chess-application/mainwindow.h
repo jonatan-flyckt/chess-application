8f39d8b56c5b3bc1afddbdd3ab8003687b099dda
jonatan-flyckt <jonatan.flyckt@hotmail.com>
2020-07-07 11:21:20 +0200

    Resize now works with kept aspect ratio and minimum board size.

--- chess-application/mainwindow.h
+++ chess-application/mainwindow.h
@@ -1,9 +1,29 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#define BOARD_GRID_COL 1
+#define BOARD_GRID_ROW 1
+
 #include <QMainWindow>
 #include <QGridLayout>
 #include <QLabel>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QDebug>
+#include <QSizePolicy>
+#include <QResizeEvent>
+
+
+class BoardAspectRatioWidget: public QWidget{
+public:
+    BoardAspectRatioWidget(QGridLayout *boardLayout, float width, float height, QWidget *parent = 0);
+    void resizeEvent(QResizeEvent *event);
+
+private:
+    QBoxLayout *layout;
+    float arWidth; // aspect ratio width
+    float arHeight; // aspect ratio height
+};
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -22,8 +42,12 @@ public:
 
 private:
     Ui::MainWindow *_ui;
-    QGridLayout *_board_layout;
+    QGridLayout *_board_grid_layout;
+    QGridLayout *_main_grid_layout;
+    BoardAspectRatioWidget *_board_aspect_ratio_widget;
 
 
 };
 #endif // MAINWINDOW_H
+
+
