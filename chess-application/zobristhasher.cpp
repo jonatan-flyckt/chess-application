6216020cf51b002f478464867862aed38b547583
jonatan-flyckt <jonatan.flyckt@hotmail.com>
2021-01-02 13:51:08 +0100

    Adds work in progress on random number generation.

--- chess-application/zobristhasher.cpp
+++ chess-application/zobristhasher.cpp
@@ -10,7 +10,7 @@ ZobristHasher::ZobristHasher(){
         _en_passant_file_numbers.push_back(generateRandomNumber());
 
     for (int i = 0; i < 64; i++)
-        cout << _piece_numbers[Piece(Black, Pawn)][i] << endl;
+        cout << _piece_numbers[Piece(Black, Pawn)].at(i) << endl;
     int hej = 1;
 }
 
@@ -19,84 +19,80 @@ ULL ZobristHasher::generateHashForPosition(BitBoard board, CastlingInfo castling
 }
 
 ULL ZobristHasher::generateRandomNumber(){
-    /*
-    srand(RANDOM_SEED);
-    int index = _current_random_index;
-    while(index-- >0)
-        rand();
+
+    std::mt19937_64 gen (std::random_device{}());
+
+    std::uint64_t randomNumber = gen();
+    cout << randomNumber << endl;
+
+    std::mt19937_64 generator(RANDOM_SEED + _current_random_index);
     _current_random_index++;
-    return rand();
-    */
-
-    thread_local std::mt19937 engine;  // or a different engine
-    engine.seed(RANDOM_SEED + _current_random_index);
-
-    _current_random_index++;
-    return engine();
-
-
+    std::uniform_int_distribution<ULL> distribution(RAND_MIN, RAND_MAX);
+    return distribution(generator);
 }
 
-map<Piece, ULL *> ZobristHasher::generateRandomNumbersForPieces(){
-    map<Piece, ULL*> pieceMap;
-    ULL numbersArr[64];
+map<Piece, vector<ULL> > ZobristHasher::generateRandomNumbersForPieces(){
+    map<Piece, vector<ULL>> pieceMap;
+    vector<ULL> numbersArr;
     for (int i = 0; i < 64; i++)
-        numbersArr[i] = generateRandomNumber();
-    pieceMap.emplace(Piece(White, Pawn), numbersArr);
+        numbersArr.push_back(generateRandomNumber());
+    pieceMap.emplace(_white_pawn, numbersArr);
+    numbersArr.clear();
 
-    numbersArr[64];
     for (int i = 0; i < 64; i++)
-        numbersArr[i] = generateRandomNumber();
-    pieceMap.emplace(Piece(White, Rook), numbersArr);
+        numbersArr.push_back(generateRandomNumber());
+    pieceMap.emplace(_white_rook, numbersArr);
+    numbersArr.clear();
 
-    numbersArr[64];
     for (int i = 0; i < 64; i++)
-        numbersArr[i] = generateRandomNumber();
-    pieceMap.emplace(Piece(White, Knight), numbersArr);
+        numbersArr.push_back(generateRandomNumber());
+    pieceMap.emplace(_white_knight, numbersArr);
+    numbersArr.clear();
 
-    numbersArr[64];
     for (int i = 0; i < 64; i++)
-        numbersArr[i] = generateRandomNumber();
-    pieceMap.emplace(Piece(White, Bishop), numbersArr);
+        numbersArr.push_back(generateRandomNumber());
+    pieceMap.emplace(_white_bishop, numbersArr);
+    numbersArr.clear();
 
-    numbersArr[64];
     for (int i = 0; i < 64; i++)
-        numbersArr[i] = generateRandomNumber();
-    pieceMap.emplace(Piece(White, Queen), numbersArr);
+        numbersArr.push_back(generateRandomNumber());
+    pieceMap.emplace(_white_queen, numbersArr);
+    numbersArr.clear();
 
-    numbersArr[64];
     for (int i = 0; i < 64; i++)
-        numbersArr[i] = generateRandomNumber();
-    pieceMap.emplace(Piece(White, King), numbersArr);
+        numbersArr.push_back(generateRandomNumber());
+    pieceMap.emplace(_white_king, numbersArr);
+    numbersArr.clear();
 
-    numbersArr[64];
     for (int i = 0; i < 64; i++)
-        numbersArr[i] = generateRandomNumber();
-    pieceMap.emplace(Piece(Black, Pawn), numbersArr);
+        numbersArr.push_back(generateRandomNumber());
+    pieceMap.emplace(_black_pawn, numbersArr);
+    numbersArr.clear();
 
-    numbersArr[64];
     for (int i = 0; i < 64; i++)
-        numbersArr[i] = generateRandomNumber();
-    pieceMap.emplace(Piece(Black, Rook), numbersArr);
+        numbersArr.push_back(generateRandomNumber());
+    pieceMap.emplace(_black_rook, numbersArr);
+    numbersArr.clear();
 
-    numbersArr[64];
     for (int i = 0; i < 64; i++)
-        numbersArr[i] = generateRandomNumber();
-    pieceMap.emplace(Piece(Black, Knight), numbersArr);
+        numbersArr.push_back(generateRandomNumber());
+    pieceMap.emplace(_black_knight, numbersArr);
+    numbersArr.clear();
 
-    numbersArr[64];
     for (int i = 0; i < 64; i++)
-        numbersArr[i] = generateRandomNumber();
-    pieceMap.emplace(Piece(Black, Bishop), numbersArr);
+        numbersArr.push_back(generateRandomNumber());
+    pieceMap.emplace(_black_bishop, numbersArr);
+    numbersArr.clear();
 
-    numbersArr[64];
     for (int i = 0; i < 64; i++)
-        numbersArr[i] = generateRandomNumber();
-    pieceMap.emplace(Piece(Black, Queen), numbersArr);
+        numbersArr.push_back(generateRandomNumber());
+    pieceMap.emplace(_black_queen, numbersArr);
+    numbersArr.clear();
 
-    numbersArr[64];
     for (int i = 0; i < 64; i++)
-        numbersArr[i] = generateRandomNumber();
-    pieceMap.emplace(Piece(Black, King), numbersArr);
+        numbersArr.push_back(generateRandomNumber());
+    pieceMap.emplace(_black_king, numbersArr);
+    numbersArr.clear();
 
+    return pieceMap;
 }
--- chess-application/zobristhasher.h
+++ chess-application/zobristhasher.h
@@ -2,6 +2,8 @@
 #define ZOBRISTHASHER_H
 
 #define RANDOM_SEED 89
+#define RAND_MAX 0xffffffffffffffff
+#define RAND_MIN 1
 
 #include "chessstructs.h"
 #include <iostream>
@@ -17,9 +19,9 @@ public:
 
     ULL generateRandomNumber();
 
-    map<Piece, ULL*> generateRandomNumbersForPieces();
+    map<Piece, vector<ULL>> generateRandomNumbersForPieces();
 
-    map<Piece, ULL*> _piece_numbers;
+    map<Piece, vector<ULL>> _piece_numbers;
 
     ULL _black_to_move_number;
 
@@ -28,6 +30,19 @@ public:
     vector<ULL> _en_passant_file_numbers;
 
     int _current_random_index;
+
+    Piece _white_pawn = Piece(White, Pawn);
+    Piece _white_rook = Piece(White, Rook);
+    Piece _white_knight = Piece(White, Knight);
+    Piece _white_bishop = Piece(White, Bishop);
+    Piece _white_queen = Piece(White, Queen);
+    Piece _white_king = Piece(White, King);
+    Piece _black_pawn = Piece(Black, Pawn);
+    Piece _black_rook = Piece(Black, Rook);
+    Piece _black_knight = Piece(Black, Knight);
+    Piece _black_bishop = Piece(Black, Bishop);
+    Piece _black_queen = Piece(Black, Queen);
+    Piece _black_king = Piece(Black, King);
 
 };
 
