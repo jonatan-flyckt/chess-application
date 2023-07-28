99ed8b94bb7186d029ead8bb4c5316b453e02266
jonatan-flyckt <jonatan.flyckt@hotmail.com>
2023-07-28 21:28:59 +0200

    Calculates 64 minus relevant bits at startup instead of every time.

--- chess-application/magicslidinggenerator.cpp
+++ chess-application/magicslidinggenerator.cpp
@@ -1,27 +1,26 @@
 #include "magicslidinggenerator.h"
 
 MagicSlidingGenerator::MagicSlidingGenerator(){
-
     for (int index = 0; index < 64; index++){
         _bishop_masks[index] = maskBishopAttacks(index);
         _rook_masks[index] = maskRookAttacks(index);
     }
-
     initaliseSliderAttacks(Bishop);
     initaliseSliderAttacks(Rook);
+
 }
 
 ULL MagicSlidingGenerator::getRookAttackRays(int index, ULL occupancy){
     occupancy &= _rook_masks[index];
     occupancy *=  _rook_magics[index];
-    occupancy >>= 64 - _rook_relevant_bits[index];
+    occupancy >>= _rook_relevant_bits[index];
     return _rook_attacks[index][occupancy];
 }
 
 ULL MagicSlidingGenerator::getBishopAttackRays(int index, ULL occupancy){
     occupancy &= _bishop_masks[index];
     occupancy *=  _bishop_magics[index];
-    occupancy >>= 64 - _bishop_relevant_bits[index];
+    occupancy >>= _bishop_relevant_bits[index];
     return _bishop_attacks[index][occupancy];
 }
 
@@ -29,19 +28,19 @@ void MagicSlidingGenerator::initaliseSliderAttacks(PieceType pieceType){
     //loop over 64 board square indices
     for (int index = 0; index < 64; index++){
         ULL mask = pieceType == Bishop ? _bishop_masks[index] : _rook_masks[index];
-        int maskBitCount = countBitsInBoard(mask);
-        int occupancy_variations = 1 << maskBitCount;
+        int bitCount = countBitsInBoard(mask);
+        int occupancyVariations = 1 << bitCount;
 
         // loop over occupancy variations, init occupancies, magic index & attacks
-        for (int count = 0; count < occupancy_variations; count++){
+        for (int count = 0; count < occupancyVariations; count++){
             if (pieceType == Bishop){
-                ULL occupancy = setOccupancy(count, maskBitCount, mask);
-                ULL magicIndex = occupancy * _bishop_magics[index] >> (64 - _bishop_relevant_bits[index]);
+                ULL occupancy = setOccupancy(count, bitCount, mask);
+                ULL magicIndex = occupancy * _bishop_magics[index] >> _bishop_relevant_bits[index];
                 _bishop_attacks[index][magicIndex] = bishopAttacksInitialisationStage(index, occupancy);
             }
             if (pieceType == Rook){
-                ULL occupancy = setOccupancy(count, maskBitCount, mask);
-                ULL magicIndex = occupancy * _rook_magics[index] >> (64 - _rook_relevant_bits[index]);
+                ULL occupancy = setOccupancy(count, bitCount, mask);
+                ULL magicIndex = occupancy * _rook_magics[index] >> _rook_relevant_bits[index];
                 _rook_attacks[index][magicIndex] = rookAttacksInitialisationStage(index, occupancy);
             }
         }
@@ -51,11 +50,11 @@ void MagicSlidingGenerator::initaliseSliderAttacks(PieceType pieceType){
 ULL MagicSlidingGenerator::setOccupancy(int index, int bitsInMask, ULL attackMask){
     ULL occupancy = 0ULL;
     for (int count = 0; count < bitsInMask; count++){
-        int index = getIndexOfLeastSignificantBit(attackMask);
-        attackMask = popBit(attackMask, index);
+        int square = getIndexOfLeastSignificantBit(attackMask);
+        attackMask = popBit(attackMask, square);
         //make sure occupancy is on board
         if (index & (1 << count))
-            occupancy |= (1ULL << index);
+            occupancy |= (1ULL << square);
     }
     return occupancy;
 }
@@ -65,16 +64,16 @@ ULL MagicSlidingGenerator::maskBishopAttacks(int index){
     ULL attacks = 0ULL;
     int file, rank;
     int targetRank = index / 8;
-    int targetFile = index % 8;
+    int targetFile= index % 8;
 
-    // generate attacks
-    for (rank = targetRank + 1, file = targetFile + 1; rank <= 6 && file <= 6; rank++, file++)
+    //generate attacks
+    for (rank = targetRank + 1, file = targetFile+ 1; rank <= 6 && file <= 6; rank++, file++)
         attacks |= (1ULL << (rank * 8 + file));
-    for (rank = targetRank + 1, file = targetFile - 1; rank <= 6 && file >= 1; rank++, file--)
+    for (rank = targetRank + 1, file = targetFile- 1; rank <= 6 && file >= 1; rank++, file--)
         attacks |= (1ULL << (rank * 8 + file));
-    for (rank = targetRank - 1, file = targetFile + 1; rank >= 1 && file <= 6; rank--, file++)
+    for (rank = targetRank - 1, file = targetFile+ 1; rank >= 1 && file <= 6; rank--, file++)
         attacks |= (1ULL << (rank * 8 + file));
-    for (rank = targetRank - 1, file = targetFile - 1; rank >= 1 && file >= 1; rank--, file--)
+    for (rank = targetRank - 1, file = targetFile- 1; rank >= 1 && file >= 1; rank--, file--)
         attacks |= (1ULL << (rank * 8 + file));
     return attacks;
 }
@@ -84,16 +83,16 @@ ULL MagicSlidingGenerator::maskRookAttacks(int index){
     ULL attacks = 0ULL;
     int file, rank;
     int targetRank = index / 8;
-    int targetFile = index % 8;
+    int targetFile= index % 8;
 
-    // generate attacks
+    //generate attacks
     for (rank = targetRank + 1; rank <= 6; rank++)
         attacks |= (1ULL << (rank * 8 + targetFile));
     for (rank = targetRank - 1; rank >= 1; rank--)
         attacks |= (1ULL << (rank * 8 + targetFile));
-    for (file = targetFile + 1; file <= 6; file++)
+    for (file = targetFile+ 1; file <= 6; file++)
         attacks |= (1ULL << (targetRank * 8 + file));
-    for (file = targetFile - 1; file >= 1; file--)
+    for (file = targetFile- 1; file >= 1; file--)
         attacks |= (1ULL << (targetRank * 8 + file));
     return attacks;
 }
@@ -103,22 +102,22 @@ ULL MagicSlidingGenerator::bishopAttacksInitialisationStage(int index, ULL block
     ULL attacks = 0ULL;
     int file, rank;
     int targetRank = index / 8;
-    int targetFile = index % 8;
+    int targetFile= index % 8;
 
-    // generate attacks
-    for (rank = targetRank + 1, file = targetFile + 1; rank <= 7 && file <= 7; rank++, file++){
+    //generate attacks
+    for (rank = targetRank + 1, file = targetFile+ 1; rank <= 7 && file <= 7; rank++, file++){
         attacks |= (1ULL << (rank * 8 + file));
         if (blockers & (1ULL << (rank * 8 + file))) break;
     }
-    for (rank = targetRank + 1, file = targetFile - 1; rank <= 7 && file >= 0; rank++, file--){
+    for (rank = targetRank + 1, file = targetFile- 1; rank <= 7 && file >= 0; rank++, file--){
         attacks |= (1ULL << (rank * 8 + file));
         if (blockers & (1ULL << (rank * 8 + file))) break;
     }
-    for (rank = targetRank - 1, file = targetFile + 1; rank >= 0 && file <= 7; rank--, file++){
+    for (rank = targetRank - 1, file = targetFile+ 1; rank >= 0 && file <= 7; rank--, file++){
         attacks |= (1ULL << (rank * 8 + file));
         if (blockers & (1ULL << (rank * 8 + file))) break;
     }
-    for (rank = targetRank - 1, file = targetFile - 1; rank >= 0 && file >= 0; rank--, file--){
+    for (rank = targetRank - 1, file = targetFile- 1; rank >= 0 && file >= 0; rank--, file--){
         attacks |= (1ULL << (rank * 8 + file));
         if (blockers & (1ULL << (rank * 8 + file))) break;
     }
@@ -130,9 +129,9 @@ ULL MagicSlidingGenerator::rookAttacksInitialisationStage(int index, ULL blocker
     ULL attacks = 0ULL;
     int file, rank;
     int targetRank = index / 8;
-    int targetFile = index % 8;
+    int targetFile= index % 8;
 
-    // generate attacks
+    //generate attacks
     for (rank = targetRank + 1; rank <= 7; rank++){
         attacks |= (1ULL << (rank * 8 + targetFile));
         if (blockers & (1ULL << (rank * 8 + targetFile))) break;
@@ -141,11 +140,11 @@ ULL MagicSlidingGenerator::rookAttacksInitialisationStage(int index, ULL blocker
         attacks |= (1ULL << (rank * 8 + targetFile));
         if (blockers & (1ULL << (rank * 8 + targetFile))) break;
     }
-    for (file = targetFile + 1; file <= 7; file++){
+    for (file = targetFile+ 1; file <= 7; file++){
         attacks |= (1ULL << (targetRank * 8 + file));
         if (blockers & (1ULL << (targetRank * 8 + file))) break;
     }
-    for (file = targetFile - 1; file >= 0; file--){
+    for (file = targetFile- 1; file >= 0; file--){
         attacks |= (1ULL << (targetRank * 8 + file));
         if (blockers & (1ULL << (targetRank * 8 + file))) break;
     }
@@ -172,6 +171,14 @@ ULL MagicSlidingGenerator::randomULL(){
     //shuffle bits and return
     return u1 | (u2 << 16) | (u3 << 32) | (u4 << 48);
 }
+
+
+
+
+
+
+
+
 
 
 
